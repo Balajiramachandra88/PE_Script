7b0197cbebd5ed0813aa174e9e3fb7b16e686053
balajiramachandra88 <bramachandra@cargomatic.com>
2022-05-10 16:41:32 +0530

    POD4

--- processors/PE_scenarios.js
+++ processors/PE_scenarios.js
@@ -1166,7 +1166,7 @@ function getPayload11(context, events, next) {
 
 function statusReady10(context, next) {
     console.log("bulk driver")
-    _parseCSV9().then((jsonObj11)=>{
+    _parseCSV11().then((jsonObj11)=>{
         let currentRow = context.vars.currentRow;
         console.log(currentRow, jsonObj11);
         let newData = data11
@@ -1214,6 +1214,7 @@ function logResponse(requestParams, response, context, ee, next) {
     console.log(parsedResponse);
     return next(); // MUST be called for the scenario to continue
 }
+
 function logResponse1(requestParams, response, context, ee, next) {
     const parsedResponse = JSON.parse(response.body);
     console.log(parsedResponse);
